e823a59befa8bc8ed99d1071569d8af041177a2f
Sandro Wenzel <sandro.wenzel@cern.ch>
2019-10-22 08:17:45 +0200

    adding vecgeomg4

---
+++ vecgeomg4.sh
@@ -0,0 +1,43 @@
+package: VecGeomG4
+version: "%(tag_basename)s"
+tag: master
+source: https://gitlab.cern.ch/VecGeom/VecGeom.git
+requires:
+  - "GCC-Toolchain:(?!osx)"
+  - "Vc"
+build_requires:
+  - CMake
+---
+
+#!/bin/bash -e
+cmake $SOURCEDIR -DCMAKE_INSTALL_PREFIX=$INSTALLROOT -DROOT=OFF \
+          -DBACKEND=Vc                                          \
+          -DVECGEOM_VECTOR=sse4.2                               \
+          -DBENCHMARK=OFF                                       \
+          ${CXXSTD:+-DCMAKE_CXX_STANDARD=$CXXSTD}               \
+          -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+
+make ${JOBS+-j $JOBS}
+make install
+
+# Modulefile
+MODULEDIR="$INSTALLROOT/etc/modulefiles"
+MODULEFILE="$MODULEDIR/$PKGNAME"
+mkdir -p "$MODULEDIR"
+cat > "$MODULEFILE" <<EoF
+#%Module1.0
+proc ModulesHelp { } {
+  global version
+  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+}
+set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
+module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+# Dependencies
+module load BASE/1.0 Vc/$VC_VERSION-$VC_REVISION ${ROOT_VERSION:+ROOT/$ROOT_VERSION-$ROOT_REVISION}
+# Our environment
+set osname [uname sysname]
+setenv VC_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
+prepend-path PATH \$::env(VC_ROOT)/bin
+prepend-path LD_LIBRARY_PATH \$::env(VC_ROOT)/lib
+$([[ ${ARCHITECTURE:0:3} == osx ]] && echo "prepend-path DYLD_LIBRARY_PATH \$::env(VC_ROOT)/lib")
+EoF
