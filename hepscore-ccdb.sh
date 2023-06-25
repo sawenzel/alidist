3f482215ff9f4adb84f4c81411400e5f88b8dcec
Sandro Wenzel <sandro.wenzel@cern.ch>
2023-06-25 10:33:51 +0200

    Using HEPscore recipe to publish ALICE3 LUT
    The HEPscore_CCDB is a recipe known to ali-publish.
    Using it (as a test) to publish ALICE3 LUT data for
    R&D studies (fast sim)

---
+++ hepscore-ccdb.sh
@@ -0,0 +1,47 @@
+package: HEPscore-CCDB
+version: alice3-lut-v0.0.1
+build_requires:
+  - xjalienfs
+  - alibuild-recipe-tools
+---
+#!/bin/bash
+
+# A build recipe publishing data files via alibuild + ali-bot publish
+
+export ROOT_INCLUDE_PATH=${ROOT_INCLUDE_PATH}:${PYTHIA_ROOT}/include
+# this is to access tokens and alien
+export PYTHONPATH=${PYTHONPATH}:${XJALIENFS_ROOT}/lib/python/site-packages
+
+alien-token-info
+RC=$?
+
+if [ "${RC}" != "0" ]; then
+ # this is for JENKINS usage (to pick up GRID certificate)
+ export X509_USER_CERT=/etc/grid-security/hostcert.pem 
+ export X509_USER_KEY=/etc/grid-security/hostkey.pem 
+ export X509_CERT_DIR=/etc/grid-security/certificates
+ export PATH=${PATH}:${XJALIENFS_ROOT}/bin
+fi
+
+env | tr ":" "\n" > runenv.log
+
+# check if we can access alien
+alien-token-info || true
+
+# get the precreated data
+alien.py cp /alice/cern.ch/user/d/ddobrigk/LUT/* file:./ALICE3-FASTSIM-LUT
+
+# install artefacts
+rsync -a --delete * $INSTALLROOT
+echo "Data files  (Delphes lookup tables) for ALICE3 R&D studies" >> $INSTALLROOT/README.txt
+
+# Modulefile --> not needed
+mkdir -p $INSTALLROOT/etc/modulefiles
+alibuild-generate-module > $INSTALLROOT/etc/modulefiles/$PKGNAME
+
+cat <<EOF >> $INSTALLROOT/etc/modulefiles/$PKGNAME
+set ALICE3-DATA_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
+setenv ALICE3-DATA_ROOT \$ALICE3-DATA_ROOT
+setenv ALICE3-DATA_VERSION $PKGVERSION
+EOF
+
